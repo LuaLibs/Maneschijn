31724a97079396936464e76a557bff4fce0f1ab2
Tricky1975 <facebook@tbbs.nl>
2018-05-06 10:22:54 +0200

    Set up alternate callback
    Notes:
    - In this setup the call to regular love callback routines still works, for the time being. This is solely crash prevention, except for love.load and love.quit all calls to love callbacks are deprecated when you call the maneschijn_callback.rel  library.
    - If you know what you are doing you can always void my callback routines and revert to your own :P
    - Nothing here has been tested. This is only the core, and later I can start writing on the actual code with which I can test everything.

--- libs/maneschijn_callback.rel/mcb.lua
+++ libs/maneschijn_callback.rel/mcb.lua
@@ -8,8 +8,122 @@
 	http://mozilla.org/MPL/2.0/.
         Version: 18.05.06
 ]]
+
+-- $USE libs/maneschijn_core
+
+local core = maneschijn_core
 local cb = { Desc='Callback'}
 
--- The actual code comes later!
+
+cb.handlers={
+}
+
+
+function love.run()
+      local mj,mi,re,cod = love.getVersion()
+      assert(mi>11 or mj>0,"GJCR6 requires LOVE 0.11 or higher")
+      -- There no need to use a different function for this
+      love.load = love.load or maan.load or maan.onload
+      love.quit = love.quit or maan.quit or maan.onquit
+      if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
+      
+      -- Only copied for safety's sake
+      -- And besides timed gadgets could need this.
+      --if love.timer then love.timer.step() end
+      assert(love.time,"Maneschijn cannot work without the love timer!")
+      love.timer.step()
+      local dt = 0       
+      
+      return function()
+        -- Process events.
+        if love.event then
+           love.event.pump()        
+           for name, a,b,c,d,e,f in love.event.poll() do
+               -- Quit process is the same
+               if name == "quit" then
+                  if not love.quit or not love.quit() then
+                     core.MainGadget.cantkill=false
+                     core.MainGadget:free()
+                     return a or 0
+                  end
+               end
+               --love.handlers[name](a,b,c,d,e,f)
+               (cb.handlers[name] or love.handlers[name])(a,b,c,d,e,f)
+               -- Please note the call to love for non-existent handlers is only a temporary measure to prevent bugs and crashes, but is deprecated from the start!
+           end
+        end
+        
+      -- Update timer value  
+      dt = love.timer.step()
+      
+      -- Update all timer based gadgets
+      core.MainGadget:UpdateTimer(dt)
+      
+      -- Draw
+      if love.graphics and love.graphics.isActive() then
+         love.graphics.origin()
+         love.graphics.clear(love.graphics.getBackgroundColor())
+         core.MainGadget:PerformDraw()
+         love.graphics.present()
+      end
+      
+
+      
+      -- The 'rest' routine
+      love.timer.sleep(0.001)
+      end
+end
 
 return {}
+
+
+
+
+--[[ 
+  Below is the ORIGINAL love.run routine....
+  It does not serve any other purpose aside from being reference material.
+  
+  
+function love.run()
+  if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
+ 
+  -- We don't want the first frame's dt to include time taken by love.load.
+  if love.timer then love.timer.step() end
+ 
+  local dt = 0
+ 
+  -- Main loop time.
+  return function()
+    -- Process events.
+    if love.event then
+      love.event.pump()
+      for name, a,b,c,d,e,f in love.event.poll() do
+        if name == "quit" then
+          if not love.quit or not love.quit() then
+            return a or 0
+          end
+        end
+        love.handlers[name](a,b,c,d,e,f)
+      end
+    end
+ 
+    -- Update dt, as we'll be passing it to update
+    if love.timer then dt = love.timer.step() end
+ 
+    -- Call update and draw
+    if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
+ 
+    if love.graphics and love.graphics.isActive() then
+      love.graphics.origin()
+      love.graphics.clear(love.graphics.getBackgroundColor())
+ 
+      if love.draw then love.draw() end
+ 
+      love.graphics.present()
+    end
+ 
+    if love.timer then love.timer.sleep(0.001) end
+  end
+end
+  
+]]
--- libs/maneschijn_core.rel/core.lua
+++ libs/maneschijn_core.rel/core.lua
@@ -10,10 +10,99 @@
 ]]
 -- Some core code will appear here, once development has begun
 
+-- $USE libs/nothing
+
+maan={} -- The only definition here, but it's used for some basic callbacks
 
 local core = {
-   copyright = "Jeroen P. Broks"
+   copyright = "Jeroen P. Broks",
+   
+   
+   -- With this you can set the number of priority layers.
+   -- With this you can determine if something is always drawn first or last.
+   -- By default I've set this to 3, but you can use any setup you like
+   -- Minimum level is 1, anything below that number is ignored.
+   -- Please note, if you have too many of these your game *is* likely to slow down
+   maxpriority=3
 }
+
+local methoden = { -- This is a bunch of methods and subvariables ALL gadgets should have
+    
+    free = function(self) -- VERY VERY important. Only release gadgets through the "free" method If you just put them to nil before releasing them you will get memory leaks do to a bug in Lua's garbage collector in 'cyclic references' in tables!
+        assert(not self.cantkill , "You tried to kill a gadget you cannot kill");
+        (self.kids.onfree or nothing)()
+        if self.kids then
+           for kid in each(self.kids) do kid:free() end -- A kid MUST have free!
+        end
+        self.parent=nil -- Very important one too!
+        self.freed=true
+        -- Now all the "dangerous stuff" is done, however keep in mind that the gadget still contains data, but if you set it to nil now, everything should be fine!
+     end,    
+     
+     UpdateTimer = function(self,dt)
+        if self.Enabled==false then return end
+        if self.Timer and self.Timer>0 then
+           self.TimerValue = (self.TimerValue or 0) + dt
+           if self.TimerValue>self.Timer then 
+              if self.id and maan[self.id] and maan[self.id].TimerAction then 
+                 maan[self.id].TimerAction(self)
+              elseif self.TimerAction then
+                 self:TimerAction()
+              end
+              self.TimerValue=0
+           end
+        end
+        for kid in each(self.kids or {}) do
+            kid:UpdateTimer()
+        end 
+     end,
+     
+     PerformDraw = function(self,prio)
+         -- Not visible? Then get outta here!
+         if self.Visible==false then return end
+         -- init
+         local priolist = {}
+         local uprio = math.ceil(self.priority) or math.ceil(core.maxpriority/2)
+         -- Add stuff into the right priority list         
+         if self.id and maan[self.id] and maan[self.id].Draw then 
+            priolist[uprio][#priolist[uprio]+1] = maan[self.id].Draw
+         elseif self.Draw then
+            priolist[uprio][#priolist[uprio]+1] = self.Draw
+         end
+         -- Recurse
+         for kid in each(self.kids or {}) do
+             local tp = kid:PerformDraw('RECURSE')
+             for ip,lp in ipairs(tp) do for f in each(lp) do priolist[ip][#priolist[ip]+1]=f end end
+         end          
+         if prio=='RECURSE' then return priolist end -- When 'RECURSE' is set, we're just gathering 
+         -- Show everything according to priority
+         assert(core.maxprio>=1,"Invalid maxprio! Must be 1 or higher!")
+         local maxp,minp=core.maxprio,1
+         if prio then maxp,minp=prio,prio end
+         for iprio = maxp,minp,-1 do
+             for d in each(priolist[iprio]) do d(self) end
+         end                             
+     end       
+}
+
+-- Is in normal setups not required in your code and called automatically when needed
+function core.AttachMethods(gadget,meths)
+    for mk,mv in pairs(meths or methoden) do
+        gadget[mk]=mv
+    end
+end    
+
+
+-- This is the 'main' gadget. Best is NOT to alter it (unless you KNOW what you are doing)
+core.MainGadget = {
+       cantkill=true,
+       x=0,y=0,
+       w=1,h=1,
+       dw='screen',dh='screen',
+       kids={} 
+}
+core.AttachMethods(core.MainGadget)
+
 
 
 return core
