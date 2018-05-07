4f70aa6fb518006b0165e2406e76dd14381f5bec
Tricky1975 <facebook@tbbs.nl>
2018-05-07 12:56:26 +0200

    debug

--- libs/maneschijn_gadgets.rel/quad.lua
+++ libs/maneschijn_gadgets.rel/quad.lua
@@ -35,7 +35,7 @@ function kquad:onCreate()
      assert(self.texture,"No texturedata found")
      self.w = self.w or 1
      self.h = self.h or 1
-     assert(self.w>0 and self.h>0,"Invalid width and height")
+     assert(self.w>0 and self.h>0,"Invalid width and height\n"..serialize("gadget",self))
      self:texload()
 end
 
