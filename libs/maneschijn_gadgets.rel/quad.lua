fd4d96f45fc565f0eb42e9ec17ab5bf10991ba2e
Tricky1975 <facebook@tbbs.nl>
2018-05-07 11:25:33 +0200

    Base code quad gadget

---
+++ libs/maneschijn_gadgets.rel/quad.lua
@@ -0,0 +1,66 @@
+--[[
+        quad.lua
+	(c) 2018 Jeroen Petrus Broks.
+	
+	This Source Code Form is subject to the terms of the 
+	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
+	distributed with this file, You can obtain one at 
+	http://mozilla.org/MPL/2.0/.
+        Version: 18.05.07
+]]
+-- $USE libs/maneschijn_core
+-- $USE libs/qgfx
+local core=maneschijn_core
+local quadmodule={}
+
+-- Methods for kquad
+local kquad = {}
+
+function kquad:initquad()
+    local x = self.insert_x or 0
+    local y = self.insert_y or 0
+    local sw,sh=ImageSizes(self.imgtexture)
+    local width,height=self.w,self.h
+    self.quadtexture = love.graphics.newQuad( x, y, width, height, sw, sh )
+end
+
+function kquad:texload(forcereload)
+    if forcereload then self.imgtexture=nil end
+    self.imgtexture = self.imgtexture or LoadImage(self.texture)
+    self:initquad()
+end
+
+function kquad:onCreate()
+     assert(self.texture,"No texturedata found")
+     self.w = self.w or 1
+     self.h = self.h or 1
+     assert(self.w>0 and self.h>0,"Invalid width and height")
+     self:texload()
+end
+
+function kquad:onResize() self:initquar() end
+
+function kquad:Draw()
+     self.frame=self.frame or 1
+     QuadImage(self.imgtexture,self.quadtexture,self.x,self.y,self.frame)
+end
+
+
+function CreateQuad(x,y,w,h,image,parent)
+     assert(x and y and w and h and image and parent,"CreateQuad: Missing parameters")
+     local quad = {
+          kind='quad',
+          x=x,
+          y=y,
+          w=w,
+          h=h,
+          image=image,
+          parent=parent,
+          kids={}          
+     }
+     CreateGadget(quad)
+     return quad
+end
+
+core.RegisterGadget("quad",kquad)
+return quadmodule
