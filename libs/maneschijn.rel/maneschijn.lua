2361d65143569efe5f29b6e473f275488822238d
Tricky1975 <facebook@tbbs.nl>
2018-05-07 11:24:51 +0200

    vUpdate

--- libs/maneschijn.rel/maneschijn.lua
+++ libs/maneschijn.rel/maneschijn.lua
@@ -6,7 +6,7 @@
 	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
 	distributed with this file, You can obtain one at 
 	http://mozilla.org/MPL/2.0/.
-        Version: 18.05.06
+        Version: 18.05.07
 ]]
 -- $USE libs/maneschijn_gadgets
 -- $USE libs/maneschijn_callback
