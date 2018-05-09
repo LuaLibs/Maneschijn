04463854c0b7b4677eb3125b8ac99129fec246a8
Tricky1975 <facebook@tbbs.nl>
2018-05-09 09:57:43 +0200

    Call to notiy library

--- libs/maneschijn.rel/maneschijn.lua
+++ libs/maneschijn.rel/maneschijn.lua
@@ -6,10 +6,14 @@
 	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
 	distributed with this file, You can obtain one at 
 	http://mozilla.org/MPL/2.0/.
-        Version: 18.05.07
+        Version: 18.05.09
 ]]
+
+-- Maneschijn libraries
 -- $USE libs/maneschijn_gadgets
 -- $USE libs/maneschijn_callback
+-- General libraries that should always come with maneschijn
+-- $USE libs/notify
 
 local schijn = {
     desc = "Just a module which can include it all!"
